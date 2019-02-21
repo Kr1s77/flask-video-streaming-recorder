3ff670e0a67956700d8a062ae3d5e8e6bbff1a5d
criseLYJ <1148995537@qq.com>
2019-02-21 09:07:16 +0800

    add login

--- readme.md
+++ readme.md
@@ -1,36 +1,76 @@
 # 树莓派+opencv+flask家庭监控系统
-## 树莓派操作
 
-- 首先树莓派上需要安装python3
-    $ sudo  apt-get  update
-    $ sudo  apt-get  upgrade
+### 首先树莓派上需要安装python3
+```
+    	$ sudo  apt-get  update
+    	$ sudo  apt-get  upgrade
+```	
+    	
 - 安装python依赖环境
+- install python Dependent environment
+```
     $ sudo apt-get install build-essential libsqlite3-dev sqlite3 bzip2 libbz2-dev
+ ```
+    
 - 下载python3.6版本源码并解压
-    $ wget https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tgz
-    $ tar zxvf Python-3.6.1.tgz
+- Download the python version 3.6 source code and decompress it
+
+```
+    	$ wget https://www.python.org/ftp/python/3.6.1/Python-3.6.1.tgz
+    	$ tar zxvf Python-3.6.1.tgz
+ ```   	
 - 编译安装
-    $ cd Python-3.6.1
-    $ sudo ./configure
-    $ sudo make
-    $ sudo make install
+- Compilation and installation
+```
+	    $ cd Python-3.6.1
+	    $ sudo ./configure
+	    $ sudo make
+	    $ sudo make install
+```	    
 - 检查安装
-    $ ls -al /usr/local/bin/python*
+- Inspection and installation
+
+
+		$ ls -al /usr/local/bin/python*
   
-## python3自带pip3
+### python3自带pip3
 
 ### 接下来安装模块
 
 - 安装flask
-    $ pip3 install flask
+- install flask
+
+
+    	$ pip3 install flask==0.10.1
+    	
 - 安装opencv
-     $ pip3 inshall opencv_python
+- install opencv
+
+
+     	$ pip3 inshall opencv_python
   
 ### 然后
+- Then
 
 - 下载所有文件运行即可
-    $ python3 serve.py
+- run main.py
+
+
+    	$ python3 main.py
+    	
+ - 2019.2.21更新
+ - 增加了登录，很简单的一个登录接口，并不需要数据库
+ - Increased login, a simple login interface, does not need a database
+ 
+ - 测试账户
+ - Test account
+ ```
+     username: admin
+     password: admin
+ 
+ ```
   
 ### 写在最后
 - 以后还会更新更多项目啊！
 - 加油吧！！！！！
+- Have a good time!
--- serve/main.py
+++ serve/main.py
@@ -1,11 +1,44 @@
-from flask import Flask, render_template, Response
-from camera import VideoCamera
+from flask import Flask, render_template, Response, request, redirect, url_for, session
 
-app = Flask(__name__)  
+from serve.camera import VideoCamera
 
+app = Flask(__name__)
+
+# session 加密秘钥
+app.secret_key = "fM3PEZwSRcbLkk2Ew82yZFffdAYsNgOddWoANdQo/U3VLZ/qNsOKLsQPYXDPon2t"
+
+
+# 主页
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # 模板渲染
+    username = session.get("username")
+    if username:
+        return render_template('index.html')
+    return redirect(url_for("login"))
+
+
+# 登录
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template("login.html")
+    # 获取参数
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    # 校验参数
+    if not all([username, password]):
+        return render_template("login.html", errmsg="参数不足")
+
+    # 校验对应的管理员用户数据
+    if not username == "admin" and password == "admin":
+        return render_template("login.html", errmsg="用户名或密码错误")
+
+    # 验证通过
+    session["username"] = username
+    return redirect(url_for("index"))
+
 
 def gen(camera):
     while True:
@@ -13,10 +46,12 @@ def gen(camera):
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
+
 @app.route('/video_feed')
 def video_feed():
     return Response(gen(VideoCamera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True,port=7000)
+    app.run(host='0.0.0.0', debug=True, port=7000)
